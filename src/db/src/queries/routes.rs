b6bac938e0f29f76c02bf4da8e07189419e3eef3
&bigbang <thebigbangfirst@gmail.com>
2020-06-02 22:10:15 +0300

    test

--- src/db/src/queries/routes.rs
+++ src/db/src/queries/routes.rs
@@ -1,16 +1,12 @@
 use crate::models::{NewRoute, Route};
 use crate::schema::routes;
-use crate::schema::routes::dsl::*;
 use crate::Repo;
 use diesel::prelude::*;
 use diesel::result::Error;
 
 pub fn insert(repo: &Repo, route: NewRoute) -> Result<Route, Error> {
-    println!("route {:?}", route);
     let route = diesel::insert_into(routes::table)
         .values(&route)
         .get_result::<Route>(&repo.conn())?;
-    let routes_rows = routes.load::<Route>(&repo.conn());
-    println!("routes_rows {:?}", routes_rows);
     Ok(route)
 }
